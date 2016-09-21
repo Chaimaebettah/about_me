2400b34c8c7ee33c5a42e291cfd0a93542ec5575
Chaimaebettah <YOUR EMAIL ADDRESS>
2016-09-21 09:34:54 -0700

    fix another prob

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //I use prompt to ask the user some questions.
-'use strict'
+'use strict';
 var guess = prompt('do you want to play a guessing game with me? say Yes to start');
 if ((guess.toLowerCase() === 'yes') || (guess.toLowerCase() === 'y')) {  
+  console.log('The user said ' + guess);
     var food = prompt('my favorite food is Snails, Yes or No?');  
+    console.log("I like snails")
     if (food.toLowerCase() === 'yes') {    
         alert('Yup, it\'s crazy but it\'s true.');  
     } else {    
@@ -13,6 +15,7 @@ if ((guess.toLowerCase() === 'yes') || (guess.toLowerCase() === 'y')) {  
 }
 
 var age = prompt('guess how old I am?');
+console.log('my age is 24 ')
 if (age < '20') {  
     alert('Ow,im not that young, I\'m 24 years old!');
 } else if ((age >= '20') && (age <= '25')) {  
@@ -22,12 +25,14 @@ if (age < '20') {  
 }
 
 var language = prompt(" my native language is Darija, Yes or No");
+console.log('my native language is Darija');
 if (language.toLowerCase() === 'yes') {
     alert(" Yes that\'s true, Darija is a Moroccan dialect of arabic.")
 } else {
     alert("Nope, it\'s  Darija")
 }
 var siblings = prompt("How many siblings do I have?");
+console.log("i have two sisters");
 if (siblings === '2') {
     alert("That\'s right, I have two sisters.")
 } else {
