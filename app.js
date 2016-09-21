4e02812c17f635d029aaa22f8927653b54983416
Chaimaebettah <YOUR EMAIL ADDRESS>
2016-09-21 07:39:24 -0700

    fix problem

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //I use prompt to ask the user some questions.
+'use strict'
 var guess = prompt('do you want to play a guessing game with me? say Yes to start');
 if ((guess.toLowerCase() === 'yes') || (guess.toLowerCase() === 'y')) {  
-    var food = prompt('what my favorite food do you think from these two? Snails or Sushi?');  
-    if (food.toLowerCase() === 'snails') {    
+    var food = prompt('my favorite food is Snails, Yes or No?');  
+    if (food.toLowerCase() === 'yes') {    
         alert('Yup, it\'s crazy but it\'s true.');  
     } else {    
         alert('No, I love eating Snails.');  
@@ -20,8 +21,8 @@ if (age < '20') {  
     alert("Im not that old, I\'m 24 years old!")
 }
 
-var language = prompt("of these two, which is my native language? Arabic or Darija?");
-if (language.toLowerCase() === 'darija') {
+var language = prompt(" my native language is Darija, Yes or No");
+if (language.toLowerCase() === 'yes') {
     alert(" Yes that\'s true, Darija is a Moroccan dialect of arabic.")
 } else {
     alert("Nope, it\'s  Darija")
